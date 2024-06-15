a3cb2d714e7cb9f204df195459b28b154db4f7de
Krishnam Agarwal <83017176+888krishnam@users.noreply.github.com>
2024-06-15 18:03:52 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import { recognize } from 'tesseract.js';
 
-const logger = (message) => {
-    console.log(message);
-};
-
-const image = document.getElementsByClassName('col-sm-4')[0].querySelector('img').src;
-console.log("image ==> ",image)
+const imgElement = document.getElementsByClassName('col-sm-4')[0].querySelector('img');
+const canvas = document.createElement('canvas');
+canvas.width = imgElement.clientWidth;
+canvas.height = imgElement.clientHeight;
+const ctx = canvas.getContext('2d');
+ctx.drawImage(imgElement, 0, 0, imgElement.clientWidth, imgElement.clientHeight);
+const image = canvas.toDataURL();
 const input = document.getElementById('ccode');
-console.log("input ==> ",input)
 
 recognize(image, 'eng', {logger: e => console.log(e)})
     .then(out => {
-        console.log("out ==> ",out.data.text)
         input.value = out.data.text;
     })
     .catch(err => {
