82e29e667e38e009607a54cdc6eb803b35c74fbc
Krishnam Agarwal <83017176+888krishnam@users.noreply.github.com>
2024-06-16 15:03:01 +0530

    Change index.js and manifest.json
    Clean up unused permissions in the extension's manifest.
    Improved: Modify index.js to read the image only after both the window and image have fully loaded in the browser. This prevents errors caused by trying to access the image before it's available.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import { recognize } from 'tesseract.js';
 
-const imgElement = document.getElementsByClassName('col-sm-4')[0].querySelector('img');
-const canvas = document.createElement('canvas');
-canvas.width = imgElement.clientWidth;
-canvas.height = imgElement.clientHeight;
-const ctx = canvas.getContext('2d');
-ctx.drawImage(imgElement, 0, 0, imgElement.clientWidth, imgElement.clientHeight);
-const image = canvas.toDataURL();
-const input = document.getElementById('ccode');
+window.addEventListener('load', () => {
+    (async () => {
+        const imgElement = document.getElementsByClassName('col-sm-4')[0].querySelector('img');
+        
+        await new Promise((resolve) => {
+            if (imgElement.complete) {
+                resolve();
+            } else {
+                imgElement.onload = resolve;
+            }
+        });
 
-recognize(image, 'eng', {logger: e => console.log(e)})
-    .then(out => {
-        input.value = out.data.text;
-    })
-    .catch(err => {
-        console.error("Error during recognition:", err);
-    });
+        const canvas = document.createElement('canvas');
+        canvas.width = imgElement.clientWidth;
+        canvas.height = imgElement.clientHeight;
+        const ctx = canvas.getContext('2d');
+        
+        ctx.drawImage(imgElement, 0, 0, imgElement.clientWidth, imgElement.clientHeight);
+        const image = canvas.toDataURL();
+
+        const input = document.getElementById('ccode');
+
+        recognize(image, 'eng', {logger: e => console.log(e)})
+            .then(out => {
+                input.value = out.data.text;
+            })
+            .catch(err => {
+                console.error("Error during recognition:", err);
+            });
+    })();
+});
